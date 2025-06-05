b9bf5222061938fda331f0387e4ffc7cf974053e
dimata <dimitar.serafimov@egt-bg.com>
2025-06-05 15:24:28 +0300

    add Present CPT

--- wordpress/wp-content/themes/sage/functions.php
+++ wordpress/wp-content/themes/sage/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 use Roots\Acorn\Application;
 
 /*
@@ -58,3 +57,51 @@ collect(['setup', 'filters'])
             );
         }
     });
+
+/*
+|--------------------------------------------------------------------------
+| Register Present CPT
+|--------------------------------------------------------------------------
+*/
+function register_presents_post_type(): void
+{
+    $labels = [
+        'name'               => 'Presents',
+        'singular_name'      => 'Present',
+        'menu_name'          => 'Presents',
+        'name_admin_bar'     => 'Present',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Present',
+        'new_item'           => 'New Present',
+        'edit_item'          => 'Edit Present',
+        'view_item'          => 'View Present',
+        'all_items'          => 'All Presents',
+        'search_items'       => 'Search Presents',
+        'parent_item_colon'  => 'Parent Presents:',
+        'not_found'          => 'No presents found.',
+        'not_found_in_trash' => 'No presents found in Trash.',
+    ];
+
+    $args = [
+        'public' => true,         // Must be true
+        'show_ui' => true,        // Ensures it's shown in the WP admin
+        'labels'             => $labels,
+        'has_archive'        => true,
+        'show_in_rest'       => true, // 🔥 Important for GraphQL and REST API
+        'rewrite'            => [ 'slug' => 'presents' ],
+        'supports'           => [ 'title', 'editor', 'thumbnail', 'custom-fields' ],
+        'menu_position'      => 20,
+        'menu_icon'          => 'dashicons-gift',
+    ];
+    register_post_type( 'presents', $args );
+}
+add_action( 'init', 'register_presents_post_type' );
+function add_graphql_to_presents_post_type( $args, $post_type ) {
+    if ( 'presents' === $post_type ) {
+        $args['show_in_graphql'] = true;
+        $args['graphql_single_name'] = 'Present';
+        $args['graphql_plural_name'] = 'Presents';
+    }
+    return $args;
+}
+add_filter( 'register_post_type_args', 'add_graphql_to_presents_post_type', 10, 2 );
